2ccf240695db4ebaa59cca10d59b4d06b3e9c273
kim344 <dongju8907@gmail.com>
2022-04-04 22:16:49 +0900

    Multi Module 샘플 코드 클론  - presentation 코드 진행중

---
+++ app/src/main/java/com/kim344/cleanarchitecturesample/di/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.kim344.cleanarchitecturesample.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application() {
+}
