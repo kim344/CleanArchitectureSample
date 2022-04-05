fbc36bb325ab1b692edcf442c557229f4629a275
kim344 <dongju8907@gmail.com>
2022-04-05 22:04:54 +0900

    Multi Module 샘플 코드 클론  - Login View, ViewModel 진행중

--- app/src/main/java/com/kim344/cleanarchitecturesample/views/login/LoginActivity.kt
+++ app/src/main/java/com/kim344/cleanarchitecturesample/views/login/LoginActivity.kt
@@ -1,4 +1,55 @@
 package com.kim344.cleanarchitecturesample.views.login
 
-class LoginActivity {
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.kim344.cleanarchitecturesample.R
+import com.kim344.cleanarchitecturesample.base.BaseActivity
+import com.kim344.cleanarchitecturesample.databinding.ActivityLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginActivity: BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    private val viewModel: LoginViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.vm = viewModel
+        initViewModelCallback()
+    }
+
+    private fun initViewModelCallback(){
+        with(viewModel){
+
+            isIdEmpty.observe(this@LoginActivity, {
+                showIdEmptyError()
+            })
+
+            isPwEmpty.observe(this@LoginActivity, {
+                showPwEmptyError()
+            })
+
+            loginErrorMsg.observe(this@LoginActivity, {
+                showToast(getString(R.string.id_pw_not_correct_error_msg))
+            })
+
+            successLogin.observe(this@LoginActivity, {
+                goMovieSearch()
+            })
+
+        }
+    }
+
+    private fun showIdEmptyError(){
+        binding.etId.error = getString(R.string.id_empty_error_msg)
+    }
+
+    private fun showPwEmptyError(){
+        binding.etPw.error = getString(R.string.pw_empty_error_msg)
+    }
+
+    private fun goMovieSearch(){
+        showToast(getString(R.string.login_success_msg))
+        //startActivity()
+        finish()
+    }
 }
--- app/src/main/java/com/kim344/cleanarchitecturesample/views/login/LoginViewModel.kt
+++ app/src/main/java/com/kim344/cleanarchitecturesample/views/login/LoginViewModel.kt
@@ -1,4 +1,43 @@
 package com.kim344.cleanarchitecturesample.views.login
 
-class LoginViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(): ViewModel() {
+
+    val id: MutableLiveData<String> = MutableLiveData("")
+    val pw: MutableLiveData<String> = MutableLiveData("")
+    private val _isIdEmpty: MutableLiveData<Unit> = MutableLiveData()
+    private val _isPwEmpty: MutableLiveData<Unit> = MutableLiveData()
+    private val _loginErrorMsg: MutableLiveData<Unit> = MutableLiveData()
+    private val _successLogin: MutableLiveData<Unit> = MutableLiveData()
+
+    val isIdEmpty: LiveData<Unit> get() = _isIdEmpty
+    val isPwEmpty: LiveData<Unit> get() = _isPwEmpty
+    val loginErrorMsg: LiveData<Unit> get() = _loginErrorMsg
+    val successLogin: LiveData<Unit> get() = _successLogin
+
+    fun onLoginClick(){
+        val id = id.value.toString().trim()
+        val pw = pw.value.toString().trim()
+
+        if (id.isEmpty()){
+            _isIdEmpty.value = Unit
+        } else if (pw.isEmpty()) {
+            _isPwEmpty.value = Unit
+        } else if (id != USER_ID || pw != USER_PW){
+            _loginErrorMsg.value = Unit
+        } else {
+            _successLogin.value = Unit
+        }
+    }
+
+    companion object { //이 아이디와 비번으로만 로그인이 가능 (서버X)
+        private const val USER_ID = "id"
+        private const val USER_PW = "pass"
+    }
 }
