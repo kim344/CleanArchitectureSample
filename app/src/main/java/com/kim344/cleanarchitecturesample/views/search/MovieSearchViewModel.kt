c297068167c49776597d8112f5d32f8e34c16700
kim344 <dongju8907@gmail.com>
2022-04-08 21:37:59 +0900

    Multi Module 샘플 코드 클론  - Movie View, ViewModel 진행중

--- app/src/main/java/com/kim344/cleanarchitecturesample/views/search/MovieSearchViewModel.kt
+++ app/src/main/java/com/kim344/cleanarchitecturesample/views/search/MovieSearchViewModel.kt
@@ -1,4 +1,11 @@
 package com.kim344.cleanarchitecturesample.views.search
 
-class MovieSearchViewModel {
+import com.kim344.cleanarchitecturesample.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MovieSearchViewModel @Inject constructor(
+
+): BaseViewModel() {
 }
