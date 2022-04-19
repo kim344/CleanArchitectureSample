b07632672517fd67531e7c8a14c66160ea12bc52
kim344 <dongju8907@gmail.com>
2022-04-19 22:49:38 +0900

    Multi Module 샘플 코드 클론  - Provides 오류 Test 진행중

--- app/src/main/java/com/kim344/cleanarchitecturesample/views/splash/SplashViewModel.kt
+++ app/src/main/java/com/kim344/cleanarchitecturesample/views/splash/SplashViewModel.kt
@@ -4,12 +4,11 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.kim344.domain.usecase.GetTestUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class SplashViewModel @Inject constructor(private val testUseCase: GetTestUseCase): ViewModel() {
+class SplashViewModel @Inject constructor(): ViewModel() {
 
     private val _goMovieSearch: MutableLiveData<Unit> = MutableLiveData()
     private val _goLogin: MutableLiveData<Unit> = MutableLiveData()
@@ -18,7 +17,6 @@ class SplashViewModel @Inject constructor(private val testUseCase: GetTestUseCas
     val goLogin: LiveData<Unit> get() = _goLogin
 
     fun doSplash() {
-        Log.e("동주","splash test = ${testUseCase.execute()}")
 //        if (getLoginUseCase.execute()){
 //            _goMovieSearch.value = Unit
 //        } else {
---
+++ domain/src/main/java/com/kim344/domain/test/TestRepository.kt
@@ -0,0 +1,5 @@
+package com.kim344.domain.test
+
+interface TestRepository {
+    var value: String
+}
