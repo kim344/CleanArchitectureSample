8d7f1fb71a9c57bbbd827bb05d280dd077481df4
kim344 <dongju8907@gmail.com>
2022-05-21 21:47:14 +0900

    MovieSearchActivity.kt Update

--- app/src/main/java/com/kim344/cleanarchitecturesample/views/search/MovieSearchActivity.kt
+++ app/src/main/java/com/kim344/cleanarchitecturesample/views/search/MovieSearchActivity.kt
@@ -1,5 +1,7 @@
 package com.kim344.cleanarchitecturesample.views.search
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.kim344.cleanarchitecturesample.R
@@ -10,6 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MovieSearchActivity: BaseActivity<ActivityMovieSearchBinding>(R.layout.activity_movie_search) {
 
+    private lateinit var movieAdapter: MovieAdapter
     private val viewModel: MovieSearchViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,4 +20,13 @@ class MovieSearchActivity: BaseActivity<ActivityMovieSearchBinding>(R.layout.act
         binding.vm = viewModel
     }
 
+    private fun initAdapter() {
+        movieAdapter = MovieAdapter { movie ->
+            Intent(Intent.ACTION_VIEW, Uri.parse(movie.link)).takeIf {
+                it.resolveActivity(packageManager) != null
+            }?.run(this::startActivity)
+        }
+
+        binding.rvMovies.adapter = movieAdapter
+    }
 }
