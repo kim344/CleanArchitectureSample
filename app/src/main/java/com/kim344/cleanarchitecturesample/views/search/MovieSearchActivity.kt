23b2f61e54ee49c3ebcc69b42120c64260230cac
kim344 <dongju8907@gmail.com>
2022-05-22 22:45:12 +0900

    MovieSearchActivity.kt & MovieSearchViewModel.kt Update

--- app/src/main/java/com/kim344/cleanarchitecturesample/views/search/MovieSearchActivity.kt
+++ app/src/main/java/com/kim344/cleanarchitecturesample/views/search/MovieSearchActivity.kt
@@ -18,6 +18,9 @@ class MovieSearchActivity: BaseActivity<ActivityMovieSearchBinding>(R.layout.act
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.vm = viewModel
+
+        initViewModelCallback()
+        initAdapter()
     }
 
     private fun initAdapter() {
@@ -28,5 +31,21 @@ class MovieSearchActivity: BaseActivity<ActivityMovieSearchBinding>(R.layout.act
         }
 
         binding.rvMovies.adapter = movieAdapter
+    }
+
+    private fun initViewModelCallback() {
+        with(viewModel) {
+            toastMsg.observe(this@MovieSearchActivity, {
+                when (toastMsg.value) {
+                    MovieSearchViewModel.MessageSet.LAST_PAGE -> showToast(getString(R.string.last_page_msg))
+                    MovieSearchViewModel.MessageSet.EMPTY_QUERY -> showToast(getString(R.string.search_input_query_msg))
+                    MovieSearchViewModel.MessageSet.NETWORK_NOT_CONNECTED -> showToast(getString(R.string.network_error_msg))
+                    MovieSearchViewModel.MessageSet.SUCCESS -> showToast(getString(R.string.load_movie_success_msg))
+                    MovieSearchViewModel.MessageSet.NO_RESULT -> showToast(getString(R.string.no_movie_error_msg))
+                    MovieSearchViewModel.MessageSet.ERROR -> showToast(getString(R.string.error_msg))
+                    MovieSearchViewModel.MessageSet.LOCAL_SUCCESS -> showToast(getString(R.string.local_db_msg))
+                }
+            })
+        }
     }
 }
--- app/src/main/java/com/kim344/cleanarchitecturesample/views/search/MovieSearchViewModel.kt
+++ app/src/main/java/com/kim344/cleanarchitecturesample/views/search/MovieSearchViewModel.kt
@@ -1,11 +1,122 @@
 package com.kim344.cleanarchitecturesample.views.search
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.kim344.cleanarchitecturesample.base.BaseViewModel
+import com.kim344.cleanarchitecturesample.utils.NetworkManager
+import com.kim344.data.utils.LAST_PAGE
+import com.kim344.domain.search.Movie
+import com.kim344.domain.usecase.GetLocalMoviesUseCase
+import com.kim344.domain.usecase.GetMoviesUseCase
+import com.kim344.domain.usecase.GetPagingMoviesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 @HiltViewModel
 class MovieSearchViewModel @Inject constructor(
-
+    private val getMovieUseCase: GetMoviesUseCase,
+    private val getPagingMoviesUseCase: GetPagingMoviesUseCase,
+    private val getLocalMoviesUseCase: GetLocalMoviesUseCase,
+    private val networkManager: NetworkManager
 ): BaseViewModel() {
+
+    private var currentQuery: String = ""
+    val query = MutableLiveData<String>()
+    private val _movieList = MutableLiveData<MutableList<Movie>>()
+    private val _toastMsg = MutableLiveData<MessageSet>()
+
+    val movieList: LiveData<MutableList<Movie>> get() = _movieList
+    val toastMsg: LiveData<MessageSet> get() = _toastMsg
+
+    fun requestMovie() {
+        currentQuery = query.value.toString().trim()
+        if (currentQuery.isEmpty()){
+            _toastMsg.value = MessageSet.EMPTY_QUERY
+            return
+        }
+
+        if (!checkNetworkState()) return
+        compositeDisposable.add(
+            getMovieUseCase.execute(currentQuery)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe { showProgress() }
+                .doAfterTerminate { hideProgress() }
+                .subscribe({ movies ->
+                    if (movies.isEmpty()){
+                        _toastMsg.value = MessageSet.NO_RESULT
+                    } else {
+                        _movieList.value = movies as ArrayList<Movie>
+                        _toastMsg.value = MessageSet.SUCCESS
+                    }
+                }, {
+                    _toastMsg.value = MessageSet.ERROR
+                })
+        )
+    }
+
+    fun requestPagingMovie(offSet: Int){
+        if (!checkNetworkState()) return
+        compositeDisposable.add(
+            getPagingMoviesUseCase.execute(currentQuery, offSet)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe { showProgress() }
+                .doAfterTerminate { hideProgress() }
+                .subscribe({ movies ->
+                    val pagingMovieList = _movieList.value
+                    pagingMovieList?.addAll(movies)
+                    _movieList.value = pagingMovieList
+                    _toastMsg.value = MessageSet.SUCCESS
+                }, {
+                    when (it.message){
+                        LAST_PAGE -> _toastMsg.value = MessageSet.LAST_PAGE
+                        else -> {
+                            _toastMsg.value = MessageSet.ERROR
+                        }
+                    }
+                })
+        )
+    }
+
+    private fun requestLocalMovies(){
+        compositeDisposable.add(
+            getLocalMoviesUseCase.execute(currentQuery)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe { showProgress() }
+                .doAfterTerminate { hideProgress() }
+                .subscribe({ movies ->
+                    if (movies.isEmpty()){
+                        _toastMsg.value = MessageSet.NETWORK_NOT_CONNECTED
+                    } else {
+                        _movieList.value = movies as ArrayList<Movie>
+                        _toastMsg.value = MessageSet.LOCAL_SUCCESS
+                    }
+                }, {
+                    _toastMsg.value = MessageSet.NETWORK_NOT_CONNECTED
+                })
+        )
+    }
+
+    private fun checkNetworkState(): Boolean {
+        return if (networkManager.checkNetworkState()) {
+            true
+        } else {
+            requestLocalMovies()
+            false
+        }
+    }
+
+    enum class MessageSet {
+        LAST_PAGE,
+        EMPTY_QUERY,
+        NETWORK_NOT_CONNECTED,
+        ERROR,
+        SUCCESS,
+        NO_RESULT,
+        LOCAL_SUCCESS
+    }
 }
