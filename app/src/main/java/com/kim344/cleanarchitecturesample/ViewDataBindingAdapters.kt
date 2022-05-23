ec25f46452ba787b1688ff0a0cf6ea42bbe83846
kim344 <dongju8907@gmail.com>
2022-05-23 20:49:18 +0900

    ViewDataBindingAdapters.kt Update

--- app/src/main/java/com/kim344/cleanarchitecturesample/ViewDataBindingAdapters.kt
+++ app/src/main/java/com/kim344/cleanarchitecturesample/ViewDataBindingAdapters.kt
@@ -1,4 +1,53 @@
 package com.kim344.cleanarchitecturesample
 
-class ViewDataBindingAdapters {
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.kim344.cleanarchitecturesample.utils.EndlessRecyclerViewScrollListener
+import com.kim344.cleanarchitecturesample.views.search.MovieAdapter
+import com.kim344.cleanarchitecturesample.views.search.MovieSearchViewModel
+import com.kim344.domain.search.Movie
+
+@BindingAdapter("htmlText")
+fun TextView.setHtmlText(html: String) {
+    text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_COMPACT)
+}
+
+@BindingAdapter("urlImage")
+fun ImageView.setUrlImage(url: String) {
+    Glide.with(this).load(url)
+        .placeholder(R.drawable.ic_default)
+        .into(this)
+}
+
+@BindingAdapter("movieRating")
+fun RatingBar.setMovieRating(score: String) {
+    rating = (score.toFloatOrNull() ?: 0f) / 2
+}
+
+@BindingAdapter("setItems")
+fun RecyclerView.setAdapterItems(items: MutableList<Movie>?) {
+    items?.let {
+        (adapter as MovieAdapter).submitList(it.toMutableList())
+    }
+}
+
+@BindingAdapter("endlessScroll")
+fun RecyclerView.setEndlessScroll(
+    viewModel: MovieSearchViewModel
+) {
+    val scrollListener =
+        object : EndlessRecyclerViewScrollListener(layoutManager as LinearLayoutManager) {
+            override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView?) {
+                viewModel.requestPagingMovie(totalItemsCount + 1)
+            }
+
+        }
+
+    addOnScrollListener(scrollListener)
 }
