5edb2a9dbee1d26941763b0e0289e327880748e6
kim344 <dongju8907@gmail.com>
2022-05-25 22:10:33 +0900

    Gradle 변경 및 RemoteModule Update RepositoryModule.kt RemoteDataModule.kt build.gradle

--- data/src/main/java/com/kim344/data/di/RemoteDataModule.kt
+++ data/src/main/java/com/kim344/data/di/RemoteDataModule.kt
@@ -1,6 +1,8 @@
 package com.kim344.data.di
 
 import com.kim344.data.api.ApiInterface
+import com.kim344.data.repository.search.remote.MovieRemoteDataSourceImpl
+import com.kim344.data.repository.search.remote.MovieRemoteDatasource
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -11,8 +13,10 @@ import javax.inject.Singleton
 @Module
 class RemoteDataModule {
 
-//    @Provides
-//    @Singleton
-//    fun provideMovieRemoteSource(apiInterface: ApiInterface): MovieRe
+    @Provides
+    @Singleton
+    fun provideMovieRemoteSource(apiInterface: ApiInterface): MovieRemoteDatasource {
+        return MovieRemoteDataSourceImpl(apiInterface)
+    }
 
 }
--- data/src/main/java/com/kim344/data/di/RepositoryModule.kt
+++ data/src/main/java/com/kim344/data/di/RepositoryModule.kt
@@ -2,7 +2,11 @@ package com.kim344.data.di
 
 import com.kim344.data.repository.login.LoginRepositoryImpl
 import com.kim344.data.repository.login.local.LoginLocalDataSource
+import com.kim344.data.repository.search.MovieRepositoryImpl
+import com.kim344.data.repository.search.local.MovieLocalDataSource
+import com.kim344.data.repository.search.remote.MovieRemoteDatasource
 import com.kim344.domain.repository.LoginRepository
+import com.kim344.domain.repository.MovieRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,6 +16,15 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 class RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideMovieRepository(
+        movieRemoteDataSource: MovieRemoteDatasource,
+        movieLocalDataSource: MovieLocalDataSource
+    ): MovieRepository {
+        return MovieRepositoryImpl(movieRemoteDataSource, movieLocalDataSource)
+    }
 
     @Provides
     @Singleton
