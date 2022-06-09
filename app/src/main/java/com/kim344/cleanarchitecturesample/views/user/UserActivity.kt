357b700a2c1d3e9fd49de5902c4456287faea615
kim344 <dongju8907@gmail.com>
2022-06-09 16:52:26 +0900

    Github RandomUser API 테스트 진행중

---
+++ app/src/main/java/com/kim344/cleanarchitecturesample/views/user/UserActivity.kt
@@ -0,0 +1,10 @@
+package com.kim344.cleanarchitecturesample.views.user
+
+import com.kim344.cleanarchitecturesample.R
+import com.kim344.cleanarchitecturesample.base.BaseActivity
+import com.kim344.cleanarchitecturesample.databinding.ActivityUserBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class UserActivity: BaseActivity<ActivityUserBinding>(R.layout.activity_user) {
+}
---
+++ app/src/main/java/com/kim344/cleanarchitecturesample/views/user/UserViewModel.kt
@@ -0,0 +1,8 @@
+package com.kim344.cleanarchitecturesample.views.user
+
+import com.kim344.cleanarchitecturesample.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class UserViewModel: BaseViewModel() {
+}
